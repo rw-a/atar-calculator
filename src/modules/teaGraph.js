3364249717fe08dcb24893a2a2b64d3f0856fdd6
rw-a <bshsphotodump@gmail.com>
2022-12-21 20:10:19 +1000

    added datapoints

--- src/modules/teaGraph.js
+++ src/modules/teaGraph.js
@@ -5,6 +5,14 @@ import ATARDATA from './../data/2021_atar_data.json'
 export default class TeaGraph extends React.Component {
     render() {
         let width = Math.min(720, document.querySelector('#root').getBoundingClientRect().width - 40);
+        
+        /* alternating text positions
+        let textPositions = [];
+        let markerPositions = [];
+        for (let i = 0; i < Object.keys(ATARDATA).length; i++) {
+            textPositions.push(["top center", "bottom center"][i % 2]);
+            markerPositions.push([0.1, -0.1][i % 2]);
+        } */
 
         return (
             <div>
@@ -12,33 +20,49 @@ export default class TeaGraph extends React.Component {
                 <Plot 
                     data={[{    // yes you need all three brackets
                         x: Object.keys(ATARDATA),
-                        y: new Array(Object.keys(ATARDATA).length).fill(0),
+                        y: Array(Object.keys(ATARDATA).length).fill(0),
+                        // y: markerPositions,
                         text: Object.values(ATARDATA),
                         type: 'scatter',
-                        textposition: 'top center',
+                        // textposition: textPositions,
+                        textposition: "top center",
                         mode: 'markers+text',
                         marker: { 
                             symbol: "line-ns",
                             size: 10,
                             line: {
                                 width: 1
-                            }},
+                            }}
                     }]}
                     layout={{
+                        title: {
+                            // text: "ATAR",
+                            font: {
+                                size: 17
+                            }
+                        },
                         width: width, 
-                        height: 240, 
+                        height: 150, 
+                        dragmode: 'pan',
                         font: {
                             family: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`
                         },
                         margin: {
-                            b: 0,
+                            b: 50,
                             l: 0,
                             r: 0,
-                            t: 0
+                            t: 10
                         },
                         xaxis: {
+                            title: {
+                                text: "TEA",
+                                font: {
+                                    size: 17
+                                }
+                            },
+                            dtick: 1,           // does nothing?
                             domain: [0, 500],
-                            // range: [1, 500],
+                            range: [486, 490],      // starting zoom, should update based on predicted ATAR
                             // showgrid: false,
                             // gridcolor: "#000000",
                             ticks: "outside",
@@ -53,7 +77,10 @@ export default class TeaGraph extends React.Component {
                         }
                     }}
                     config={{
-                        scrollZoom: true
+                        scrollZoom: true,
+                        displaylogo: false,
+                        // displayModeBar: false,
+                        modeBarButtonsToRemove: ["select2d", "lasso2d"]
                     }}
                 />
             </div>
