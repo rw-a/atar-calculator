5677df157c65f3dd4e98dc11584f5578d904dd61
rw-a <rwa2684@gmail.com>
2023-03-01 22:18:28 +1000

    Updates

---
+++ precache-manifest.2005c857f43dc0213d6d3551215aa8d3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a39c12abb9eec09a98",
+    "url": "/atar-calculator/static/css/main.8a9f4c05.chunk.css"
+  },
+  {
+    "revision": "d2a39c12abb9eec09a98",
+    "url": "/atar-calculator/static/js/main.d2a39c12.chunk.js"
+  },
+  {
+    "revision": "1de2eff420fca937c213",
+    "url": "/atar-calculator/static/css/1.d5152c3a.chunk.css"
+  },
+  {
+    "revision": "1de2eff420fca937c213",
+    "url": "/atar-calculator/static/js/1.1de2eff4.chunk.js"
+  },
+  {
+    "revision": "49c897e134fedb44ee2d",
+    "url": "/atar-calculator/static/js/2.49c897e1.chunk.js"
+  },
+  {
+    "revision": "1ca5e2e14363bd6376df",
+    "url": "/atar-calculator/static/js/3.1ca5e2e1.chunk.js"
+  },
+  {
+    "revision": "49e1444104f323b205b7",
+    "url": "/atar-calculator/static/js/4.49e14441.chunk.js"
+  },
+  {
+    "revision": "deb87853fc1a5ca651ac",
+    "url": "/atar-calculator/static/js/runtime~main.deb87853.js"
+  },
+  {
+    "revision": "8191094cb987cb3e239a78200fce2150",
+    "url": "/atar-calculator/static/media/help.8191094c.svg"
+  },
+  {
+    "revision": "5dc1d189e57bec49df51b98b23525513",
+    "url": "/atar-calculator/static/media/save.5dc1d189.svg"
+  },
+  {
+    "revision": "036224a09945d2a20b5fda692b6198f5",
+    "url": "/atar-calculator/static/media/save_filled.036224a0.svg"
+  },
+  {
+    "revision": "86376276ca4d2bcdce8a45e2272aa3df",
+    "url": "/atar-calculator/static/media/delete.86376276.svg"
+  },
+  {
+    "revision": "aeced1493e8b44d507e352a0b69364c5",
+    "url": "/atar-calculator/static/media/delete_filled.aeced149.svg"
+  },
+  {
+    "revision": "14aac041c81bdf2ad943339eb1ff1f06",
+    "url": "/atar-calculator/index.html"
+  }
+];
