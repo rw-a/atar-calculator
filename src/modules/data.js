57d0cf352ffc2b13e3775c0e7dfe1ba8f4824527
rw-a <rwa2684@gmail.com>
2023-03-23 20:56:33 +1000

    updated data

--- src/modules/data.js
+++ src/modules/data.js
@@ -59,7 +59,7 @@ export function estimateAtarModel(tea, year) {
     } else if (yearStr === "2021") {
         return -0.0000971137 * (tea ** 2) + 0.251785 * tea - 0.000701489;
     } else if (yearStr === "2022") {
-        return -0.0000862361 * (tea ** 2) + 0.246812 * tea + 0.00832645;
+        return -0.0000866052 * (tea ** 2) + 0.247 * tea + 0.00542687;
     } else {
         console.error(`Invalid year at predictAtarModel(). Year=${year}`);
     }
